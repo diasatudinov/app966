12041cbe47f1e14f5e3ca9a1d17fe39bc0096b3c
Dias <dias.atudinov@gmail.com>
2024-10-21 18:54:44 +0500

    new files

---
+++ app966/app966/First/HomeView/Model/Home.swift
@@ -0,0 +1,8 @@
+//
+//  Home.swift
+//  app966
+//
+//  Created by Dias Atudinov on 21.10.2024.
+//
+
+import Foundation
---
+++ app966/app966/First/HomeView/ViewModel/HomeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  HomeViewModel.swift
+//  app966
+//
+//  Created by Dias Atudinov on 21.10.2024.
+//
+
+import Foundation
