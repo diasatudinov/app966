8ae4ad6ec50a2b3424e6dc802c71eafc1fd1bf46
Dias <dias.atudinov@gmail.com>
2024-10-21 22:10:01 +0500

    Calendar view model

--- app966/app966/First/CalendarView/ViewModel/CalendarViewModel.swift
+++ app966/app966/First/CalendarView/ViewModel/CalendarViewModel.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+
+class CalendarViewModel: ObservableObject {
+    @Published var calendars: [Calendar] = [
+//        Calendar(date: "December 25, 2023", location: "Miami, USA", height: "3,2 Metrs", equipments: ["Surfboard", "Wetsuit"]),
+//        Calendar(date: "December 25, 2023", location: "Miami, USA", height: "3,2 Metrs", equipments: ["Surfboard", "Wetsuit"]),
+//        Calendar(date: "December 25, 2023", location: "Miami, USA", height: "3,2 Metrs", equipments: ["Surfboard", "Wetsuit"]),
+//        Calendar(date: "December 25, 2023", location: "Miami, USA", height: "3,2 Metrs", equipments: ["Surfboard", "Wetsuit"]),
+//        Calendar(date: "December 25, 2023", location: "Miami, USA", height: "3,2 Metrs", equipments: ["Surfboard", "Wetsuit"]),
+//        Calendar(date: "December 25, 2023", location: "Miami, USA", height: "3,2 Metrs", equipments: ["Surfboard", "Wetsuit"]),
+
+
+    
+    ]
+    
+    func addCalendar(_ calendar: Calendar) {
+        calendars.append(calendar)
+    }
+    
+    func deleteCalendar(_ calendar: Calendar) {
+        if let index = calendars.firstIndex(where: { $0.id == calendar.id }) {
+            calendars.remove(at: index)
+        }
+    }
+}
