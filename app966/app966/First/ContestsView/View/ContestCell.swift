a7cddbaff18d9ed15404428fe8add2dc5678d5e4
Dias <dias.atudinov@gmail.com>
2024-10-22 15:00:08 +0500

    Contest cell view

--- app966/app966/First/ContestsView/View/ContestCell.swift
+++ app966/app966/First/ContestsView/View/ContestCell.swift
@@ -8,11 +8,71 @@
 import SwiftUI
 
 struct ContestCell: View {
+    @State var contest: Contest
+    var onDelete: () -> Void
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Rectangle()
+                .foregroundColor(.primaryRed)
+            VStack {
+                HStack {
+                    Spacer()
+                    Image("cellImage")
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+            
+            VStack(alignment: .leading, spacing: 0) {
+                VStack(alignment: .leading, spacing: 10) {
+                    HStack {
+                        Text(contest.date)
+                            .font(.system(size: 16, weight: .semibold))
+                        
+                        Spacer()
+                        Button {
+                            onDelete()
+                        } label: {
+                            Image(systemName: "trash.fill")
+                        }
+                    }.foregroundColor(.white)
+                    HStack {
+                        Image(systemName: "beach.umbrella")
+                            .font(.system(size: 15, weight: .regular))
+                        Text(contest.location)
+                            .font(.system(size: 17, weight: .semibold))
+                            .opacity(0.7)
+                        Spacer()
+                    }.foregroundColor(.white)
+                    
+                    HStack {
+                        Image(systemName: "surfboard")
+                            .font(.system(size: 15, weight: .regular))
+                        Text(contest.height)
+                            .font(.system(size: 17, weight: .semibold))
+                            .opacity(0.7)
+                        Spacer()
+                    }.foregroundColor(.white)
+                }.padding(.bottom, 25)
+                
+                VStack(alignment: .leading, spacing: 10) {
+                    HStack {
+                        Text(contest.note)
+                            .multilineTextAlignment(.leading)
+                            .font(.system(size: 13, weight: .regular))
+                            .foregroundColor(.white.opacity(0.7))
+                        Spacer()
+                    }
+                    
+                    
+                }
+                Spacer()
+                
+            }.padding()
+        }.frame(height: 203).cornerRadius(20)
     }
 }
 
 #Preview {
-    ContestCell()
+    ContestCell(contest: Contest(date: "December 25, 2023", location: "Miami, USA", height: "3,2 Metrs", note: "This season, broadcasts were organized for fans at almost every competition location. The upcoming races on Elbrus can be watched live on March 31 and April 1 in the official VKontakte groups of the Russian Alpine Ski Federation and Kavkaz.RF."), onDelete: {})
 }
